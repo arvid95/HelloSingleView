8d47252abb5663250872907bb93899b91cd00664
Arvid <arvidaxelsson1995@gmail.com>
2019-10-25 11:42:54 +0200

    Created new version

---
+++ HelloSingleView2.0/HelloSingleView2.0/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloSingleView2.0
+//
+//  Created by Arvid Axelsson on 2019-10-25.
+//  Copyright © 2019 Arvid Axelsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClicked(_ sender: Any)
+    {
+        print("Hello!")
+    }
+    
+}
+
