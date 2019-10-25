2e0cf9f2ae3b275a742db61df0231d754430e632
Arvid <arvidaxelsson1995@gmail.com>
2019-10-25 10:56:51 +0200

    Added "Hello" button

---
+++ HelloSingleView/HelloSingleView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Arvid Axelsson on 2019-10-25.
+//  Copyright © 2019 Arvid Axelsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClick(_ sender: Any)
+    {
+        print("Hello-button!")
+    }
+    
+}
+
